da7f79b4f90355574300edf279668a3eddbf4ec7
Clément Hélardot <clement.helardot@gmail.com>
2018-06-06 20:47:53 +0200

    added async example

---
+++ clement_test/async_example.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+//npm install node-fetch
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(json => {console.log(json)
+    sayHi("Clément"); // function call
+  })
+  .catch((err) => bad())
+  .finally(onFinally);
+
+
+function onFinally(){
+    console.log('Finally')
+}
+
+//function def
+function sayHi(name){
+  console.log(`Hi ${name}`)
+}
+
+function bad(){
+  console.log('Something went wrong here')
+}

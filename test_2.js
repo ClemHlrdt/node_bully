4f86c0748dfe4f963c0e33fbcf5ef915c2b529d1
Clément Hélardot <clement.helardot@gmail.com>
2018-06-06 14:58:19 +0200

    Removed test files

--- test_2.js
+++
@@ -1,9 +0,0 @@
-var Set = require("collections/set");
-
-var set = new Set();
-console.log(set);
-set.add([1,2,3]);
-console.log(set);
-set.add([4,5,6]);
-console.log(set);
-console.log(iterator(set));
